944b6e902122c1587f39f1fceb8115a00269ec25
Averagegael <michaelle@sunrisemiddle.org>
2025-11-24 09:52:55 -0800

    Create scramjet.config.js

---
+++ scramjet.config.js
@@ -0,0 +1,8 @@
+// Minimal Scramjet frontend config
+self.__scramjet = {
+  backend: "https://scramjet.uraverageopdoge.workers.dev/", // Your Cloudflare Worker backend
+  bareMux: {
+    version: 1,
+    path: "/bare/",
+  },
+};
