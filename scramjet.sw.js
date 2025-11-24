5ef50dc24966075445bbfc64c1231984acca047e
Averagegael <michaelle@sunrisemiddle.org>
2025-11-24 10:52:52 -0800

    Delete scramjet.sw.js

--- scramjet.sw.js
+++
@@ -1,5 +0,0 @@
-// Minimal service worker to activate Scramjet
-self.addEventListener("fetch", (event) => {
-  // For testing, just forward all requests
-  event.respondWith(fetch(event.request));
-});
