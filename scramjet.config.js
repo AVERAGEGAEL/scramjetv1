f4146cc8c69faa836aaeacd06cafa909cc5ffbce
Averagegael <michaelle@sunrisemiddle.org>
2025-11-24 10:52:30 -0800

    Delete scramjet.config.js

--- scramjet.config.js
+++
@@ -1,8 +0,0 @@
-// Minimal Scramjet frontend config
-self.__scramjet = {
-  backend: "https://scramjet.uraverageopdoge.workers.dev/", // Your Cloudflare Worker backend
-  bareMux: {
-    version: 1,
-    path: "/bare/",
-  },
-};
